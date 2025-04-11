378a672179359c4873b0bbf9b81896357f98a213
Jelle van der Waa <jelle@vdwaa.nl>
2025-04-11 10:18:30 +0200

    tests: port test_scanner to pytest

--- varlink/tests/test_scanner.py
+++ varlink/tests/test_scanner.py
@@ -1,11 +1,10 @@
-import unittest
+import pytest
 
 import varlink
 
 
-class TestScanner(unittest.TestCase):
-    def test_scanner_1(self):
-        interface = varlink.Interface("""# Example Varlink service
+def test_scanner_1():
+    interface = varlink.Interface("""# Example Varlink service
 interface org.example.more
 
 # Enum, returning either start, progress or end
@@ -35,89 +34,102 @@ type ErrorChain (
 
 error ActionFailed (reason: ?ErrorChain)
 """)
-        self.assertEqual(interface.name, "org.example.more")
-        self.assertIsNotNone(interface.get_method("Ping"))
-        self.assertIsNotNone(interface.get_method("TestMore"))
-        self.assertIsNotNone(interface.get_method("TestMap"))
-        self.assertIsNotNone(interface.get_method("StopServing"))
-        self.assertIsInstance(interface.members.get("ActionFailed"), varlink.scanner._Error)
-        self.assertIsInstance(interface.members.get("State"), varlink.scanner._Alias)
+    assert interface.name == "org.example.more"
+    assert interface.get_method("Ping") is not None
+    assert interface.get_method("TestMore") is not None
+    assert interface.get_method("TestMap") is not None
+    assert interface.get_method("StopServing") is not None
+    assert isinstance(interface.members.get("ActionFailed"), varlink.scanner._Error)
+    assert isinstance(interface.members.get("State"), varlink.scanner._Alias)
 
-    def test_doubleoption(self):
-        interface = None
-        try:
-            interface = varlink.Interface("""
-    interface org.example.doubleoption
-    method Foo(a: ??string) -> ()
-    """)
-        except SyntaxError:
-            pass
 
-        self.assertIsNone(interface)
-
-    def test_complex(self):
+def test_doubleoption():
+    interface = None
+    try:
         interface = varlink.Interface("""
-    interface org.example.complex
-    
-    type TypeEnum ( a, b, c )
-    
-    type TypeFoo (
-        bool: bool,
-        int: int,
-        float: float,
-        string: ?string,
-        enum: ?[]( foo, bar, baz ),
-        type: ?TypeEnum,
-        anon: ( foo: bool, bar: int, baz: [](a: int, b: int) ),
-        object: object
-    )
-    
-    method Foo(a: (b: bool, c: int), foo: TypeFoo) -> (a: [](b: bool, c: int), foo: TypeFoo)
-    
-    error ErrorFoo (a: (b: bool, c: int), foo: TypeFoo)
-    """)
-        self.assertEqual(interface.name, "org.example.complex")
-        self.assertIsNotNone(interface.get_method("Foo"))
-        self.assertIsInstance(interface.members.get("ErrorFoo"), varlink.scanner._Error)
-        self.assertIsInstance(interface.members.get("TypeEnum"), varlink.scanner._Alias)
+interface org.example.doubleoption
+method Foo(a: ??string) -> ()
+""")
+    except SyntaxError:
+        pass
 
-    def test_interfacename(self):
-        self.assertRaises(SyntaxError, varlink.Interface, "interface .a.b.c\nmethod F()->()")
-        self.assertRaises(
-            SyntaxError, varlink.Interface, "interface com.-example.leadinghyphen\nmethod F()->()"
-        )
-        self.assertRaises(
-            SyntaxError, varlink.Interface, "interface com.example-.danglinghyphen-\nmethod F()->()"
-        )
-        self.assertRaises(
-            SyntaxError, varlink.Interface, "interface co9.example.number-toplevel\nmethod F()->()"
-        )
-        self.assertRaises(
-            SyntaxError, varlink.Interface, "interface 1om.example.number-toplevel\nmethod F()->()"
-        )
-        self.assertRaises(SyntaxError, varlink.Interface, "interface ab\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface .a.b.c\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface a.b.c.\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface a..b.c\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface 1.b.c\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface 8a.0.0\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface -a.b.c\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface a.b.c-\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface a.b-.c-\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface a.-b.c-\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface a.-.c\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface a.*.c\nmethod F()->()")
-        self.assertRaises(SyntaxError, varlink.Interface, "interface a.?\nmethod F()->()")
-        self.assertIsNotNone(varlink.Interface("interface a.b\nmethod F()->()").name)
-        self.assertIsNotNone(varlink.Interface("interface a.b.c\nmethod F()->()").name)
-        self.assertIsNotNone(varlink.Interface("interface a.1\nmethod F()->()").name)
-        self.assertIsNotNone(varlink.Interface("interface a.0.0\nmethod F()->()").name)
-        self.assertIsNotNone(varlink.Interface("interface org.varlink.service\nmethod F()->()").name)
-        self.assertIsNotNone(varlink.Interface("interface com.example.0example\nmethod F()->()").name)
-        self.assertIsNotNone(varlink.Interface("interface com.example.example-dash\nmethod F()->()").name)
-        self.assertIsNotNone(
-            varlink.Interface("interface xn--lgbbat1ad8j.example.algeria\nmethod F()->()").name
-        )
-        self.assertIsNotNone(
-            varlink.Interface("interface xn--c1yn36f.xn--c1yn36f.xn--c1yn36f\nmethod F()->()").name
-        )
+    assert interface is None
+
+
+def test_complex():
+    interface = varlink.Interface("""
+interface org.example.complex
+
+type TypeEnum ( a, b, c )
+
+type TypeFoo (
+    bool: bool,
+    int: int,
+    float: float,
+    string: ?string,
+    enum: ?[]( foo, bar, baz ),
+    type: ?TypeEnum,
+    anon: ( foo: bool, bar: int, baz: [](a: int, b: int) ),
+    object: object
+)
+
+method Foo(a: (b: bool, c: int), foo: TypeFoo) -> (a: [](b: bool, c: int), foo: TypeFoo)
+
+error ErrorFoo (a: (b: bool, c: int), foo: TypeFoo)
+""")
+
+    assert interface.name == "org.example.complex"
+    assert interface.get_method("Foo") is not None
+    assert isinstance(interface.members.get("ErrorFoo"), varlink.scanner._Error)
+    assert isinstance(interface.members.get("TypeEnum"), varlink.scanner._Alias)
+
+
+def test_interfacename():
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface .a.b.c\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface com.-example.leadinghyphen\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface com.example-.danglinghyphen-\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface co9.example.number-toplevel\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface 1om.example.number-toplevel\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface ab\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface .a.b.c\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface a.b.c.\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface a..b.c\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface 1.b.c\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface 8a.0.0\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface -a.b.c\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface a.b.c-\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface a.b-.c-\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface a.-b.c-\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface a.-.c\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface a.*.c\nmethod F()->()")
+    with pytest.raises(SyntaxError):
+        varlink.Interface("interface a.?\nmethod F()->()")
+
+    assert varlink.Interface("interface a.b\nmethod F()->()").name is not None
+    assert varlink.Interface("interface a.b.c\nmethod F()->()").name is not None
+    assert varlink.Interface("interface a.1\nmethod F()->()").name is not None
+    assert varlink.Interface("interface a.0.0\nmethod F()->()").name is not None
+    assert varlink.Interface("interface org.varlink.service\nmethod F()->()").name is not None
+    assert varlink.Interface("interface com.example.0example\nmethod F()->()").name is not None
+    assert varlink.Interface("interface com.example.example-dash\nmethod F()->()").name is not None
+    assert varlink.Interface("interface xn--lgbbat1ad8j.example.algeria\nmethod F()->()").name is not None
+    assert (
+        varlink.Interface("interface xn--c1yn36f.xn--c1yn36f.xn--c1yn36f\nmethod F()->()").name is not None
+    )
